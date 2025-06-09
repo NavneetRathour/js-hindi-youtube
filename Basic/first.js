c9a282637f566e3e0badd233193ff47a1a843579
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-09 23:43:50 +0000

    Datatype

---
+++ Basic/first.js
@@ -0,0 +1,26 @@
+// const accountId  =143567;
+// let  accountEmail="navneet@gmail.com";
+// var accountPassward= "123456"
+
+// console.table([accountId, accountEmail,accountPassward])
+
+/*
+prefer not to use var because of issue in block scopend functional scope 
+
+use strict => Treat all js code as newer verson 
+code readabilitiy should be high
+*/
+
+// Data types
+// number => 2 to power 53
+// bigint
+//String => ""
+// boolean => True/False
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
