91c6799345fb0df1b7fdb944482d3776c499a4dd
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-11 13:16:55 +0000

    comparition

---
+++ Basic/Datatype.js
@@ -0,0 +1,34 @@
+
+// primitve
+
+// 7 types => String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const  score= 100;
+const scorevalue= 100.3;
+ 
+const IsLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id =Symbol('123');
+const another=Symbol('123')
+
+console.log(id === another);
+
+const biginteger= 3589789654217997n;
+
+console.log(typeof biginteger);
+
+// Reference (non primitive)
+// Array Function,object
+
+const array=["shaktimal", "krish","doga"];
+
+let myobj={
+    name: "Navneet Kumar",
+    age: 22,
+}
+
+console.log(typeof array);
+console.log(typeof myobj);
+console.log(typeof another);
