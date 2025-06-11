4e94021d019b8433318c388412fc13ec70fa1f08
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-11 08:46:18 +0000

    operation

--- Basic/convertion.js
+++ Basic/convertion.js
@@ -5,8 +5,8 @@ let score="Navneet"
 // console.log(typeof(score));
 
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33"=>33
 // "33abc"=>Nan
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn="Navneet"//"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => True 0=>False
 // "" => False
@@ -24,8 +24,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let StringNumber = String(someNumber);
-console.log(typeof StringNumber);
-console.log(StringNumber);
+// console.log(typeof StringNumber);
+// console.log(StringNumber);
 
 let name= "Navneet";
+
+// ***************************Operatins*********************************
+
+console.log(1+1)
+console.log(1-1)
+console.log(1*1)
+console.log(1**1)
+console.log(1%1)
+console.log(1/1)
+
+console.log("1" + 1);
+console.log(1 + "1" );
+console.log("1"+ 1 +1);
+console.log(1+ 1 +"1");
+
+console.log((3+4) *5 % 6);
+
+console.log(+true)
+
+let counter = 100;
+counter++;
+
+
+console.log(counter);
+
 
