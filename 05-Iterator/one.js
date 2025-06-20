4dfccc9b20b92432fd7dc31211ddb435c93b2dc9
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-20 16:53:23 +0000

    loop

---
+++ 05-Iterator/one.js
@@ -0,0 +1,13 @@
+// ctrl+d duplicate krege 
+
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element== 5) {
+        console.log("5 is beat  eliment");
+        
+    }
+    console.log(element);
+    
+}
+// console.log(element); //=> scope ke bahar to chalega hi nhi
