e3bc1c9b78fe77c7de3ac6ea506c60447714543a
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-21 11:47:10 +0000

    loops

--- 05-Iterator/one.js
+++ 05-Iterator/one.js
@@ -1,13 +1,55 @@
 // ctrl+d duplicate krege 
 
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element== 5) {
-        console.log("5 is beat  eliment");
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element== 5) {
+//         console.log("5 is beat  eliment");
         
-    }
+//     }
+//     console.log(element);
+    
+// }
+// console.log(element); //=> scope ke bahar to chalega hi nhi
+
+// for (let i = 0; i<= 10; i++) {
+    
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//       //  console.log(`inner loop value${j} and inner loop ${i}`);
+//         console.log(i + '*' + j  +' = ' + i*j );
+        
+        
+        
+//     }
+    
+// }
+
+let myArray = ["batman", "superman", "flash"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
     console.log(element);
     
+    
 }
-// console.log(element); //=> scope ke bahar to chalega hi nhi
+
+// break and continue
+
+for(let i=0; i<= 10; i++){
+
+      if (i==5) {
+        console.log(`detected 5`);
+        
+        break
+      }
+      console.log(`value of i is ${i}`);
+}
+for(let i=0; i<= 10; i++){
+
+      if (i==5) {
+        console.log(`detected 5`);
+        
+        continue
+      }
+      console.log(`value of i is ${i}`);
+}
