650dcee86d6d624643e5e3ec0b051b616f2b5749
NavneetRathour <navneetkumar969605@gmail.com>
2025-06-22 08:53:13 +0000

    work

--- 05-Iterator/three.js
+++ 05-Iterator/three.js
@@ -1,3 +1,18 @@
 
 //["", "", "" ]
 // [{},{},{}]
+
+const arr  = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greetings =  "hello world"
+
+for (const greetg of  greetings) {
+    
+    console.log(`Each charactor is ${greetg}`);
+    
+}
